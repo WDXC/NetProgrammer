b92c3ca4ac4f285478191b78776273f83f487787
WDXC <1508498108zty@gmail.com>
2023-06-26 19:26:47 +0800

    update chatserver

--- src/Chat_Server/ChatServer.cpp
+++ src/Chat_Server/ChatServer.cpp
@@ -4,27 +4,55 @@
 #include <event2/buffer.h>
 #include <event2/listener.h>
 #include <event2/util.h>
+#include <string>
+#include <vector>
 
 namespace Chat {
+
+
+struct Client {
+    struct bufferevent* bev;
+};
+
+
+std::vector<Client> clients;
 
 ChatServer::ChatServer() {}
 
 ChatServer::~ChatServer() {}
 
-void ChatServer::on_read_cb(struct bufferevent* bev, void* ctx) {
-    struct evbuffer* input = bufferevent_get_input(bev);
+void send_message_to_clients(const std::string& message,  struct bufferevent* sender) {
+    for (const auto& client : clients) {
+        if (client.bev != sender) {
+        struct evbuffer* output = bufferevent_get_output(client.bev);
+        evbuffer_add(output, message.c_str(), message.size());
+        bufferevent_flush(client.bev, EV_WRITE, BEV_FLUSH);
+
+        }
+    }
+}
+
+void ChatServer::on_read_cb( struct bufferevent *bev, void* ctx) {
+       struct evbuffer* input = bufferevent_get_input(bev);
     struct evbuffer* output = bufferevent_get_output(bev);
 
-    // read from the buffer
+    // 从输入缓冲区读取数据
     size_t len = evbuffer_get_length(input);
-    char* data = new char[len+1];
+    char* data = new char[len + 1];
     evbuffer_copyout(input, data, len);
+    data[len] = '\0';
 
-    // write data to buffer
-    evbuffer_add_buffer(output, input);
+    // 将数据写入输出缓冲区
+    evbuffer_add(output, data, len);
 
-    // release memory
-    delete [] data;
+    // 释放内存
+    delete[] data;
+
+    evbuffer_drain(input, len);
+
+    // 向其他客户端发送消息
+    std::string message(data, len);
+    send_message_to_clients(message, bev);
 }
 
 void ChatServer::echo_event_cb(struct bufferevent* bev, short events, void* ctx) {
@@ -43,11 +71,15 @@ void ChatServer::on_accpet_cb(struct evconnlistener* listener,
     struct bufferevent* bev = 
         bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
 
+
     // set read callback and event callback
     bufferevent_setcb(bev, on_read_cb, NULL, echo_event_cb, NULL);
 
     // start monitor the event
     bufferevent_enable(bev, EV_READ | EV_WRITE);
+    Client tmp;
+    tmp.bev = bev;
+    clients.push_back(tmp);
 }
 
 void ChatServer::accept_error_cb(struct evconnlistener* listener, void* ctx) {
@@ -91,6 +123,5 @@ void ChatServer::init() {
     evconnlistener_free(listener);
     event_base_free(base);
 }
-
 
 } // namespace Chat
